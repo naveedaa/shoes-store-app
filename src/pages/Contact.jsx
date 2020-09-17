98370d6d9b6f67a1bf9645ffef46e099af31e0e5
eraj <erajhanif9@gmail.io>
2020-09-18 00:05:45 +0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <Layout title="Contact" description="This is the Contact page" >
             <div className="text-center mt-5 contact" style={{ marginBottom:"29%"}}>
                 <h1 style={{color: "grey"}} >Contact Query</h1>
-                <a className="btn btn-primary" href="https://github.com/naveedaa?tab=repositories">
+                <a className="btn btn-primary" href="https://github.com/naveedaa/shoes-store-app">
                     <GitHubIcon width={"18px"}/> 
                         <span className="ml-2 mr-4">
                             Visit Repo
